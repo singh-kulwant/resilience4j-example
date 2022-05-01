28c4abda5e79d74af5686da99a4cca6b1e517810
singh-kulwant <singh.kulwant@gmx.com>
2022-05-01 02:11:44 +0200

    05-01: initial commit

---
+++ src/test/java/com/kulsin/Resilience4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kulsin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
